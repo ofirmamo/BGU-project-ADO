eed4690671257fcce8b23b41003893471cac283d
dalitroz <dalitrozin@gmail.com>
2018-03-03 16:44:22 +0200

    working client files

--- 3-tier application/clients/Client.py
+++ 3-tier application/clients/Client.py
@@ -8,15 +8,14 @@ class Client:
         while i < 100:
             requests.get(address)
             i += 1
-            print('got')
 
     def post_request(self, address, init_user):
         current_user = init_user
         i = 0
         while i < 100:
             mail = current_user + '@ado.com'
-            requests.post(address, data={current_user, mail})
-            current_user = current_user + current_user
+            requests.post(address, data={current_user: mail})
+            current_user = current_user + 'a'
             i += 1
             print('posted')
 
@@ -25,7 +24,7 @@ class Client:
         i = 0
         while i < 100:
             mail = current_user + '@ado.com'
-            requests.delete(address, data={current_user, mail})
-            current_user = current_user + current_user
+            requests.delete(address, data={current_user: mail})
+            current_user = current_user + 'a'
             i += 1
-            print('posted')
+            print('deleted')
--- 3-tier application/clients/Clients.py
+++ 3-tier application/clients/Clients.py
@@ -3,29 +3,20 @@ import threading
 import Client
 
 
-def request(address):
-
-    i = 0
-    while i < 10:
-        i += 1
-        response = requests.get(address)
-        print(response.url)  # remove later - check pritn
-
-
 def main():
-    address = "http://www.google.com"
-    # threads = []
-    client = Client.Client()
-    # for j in range(7):
-    t1 = threading.Thread(target=client.get_request(address))
-    t2 = threading.Thread(target=client.post_request(address, 'a'))
-    t3 = threading.Thread(target=client.delete_request(address, 'a'))
+    address = "http://127.0.0.1:5000"
 
-    # threads.append(t)
+    client = Client.Client()
+    t1 = threading.Thread(target=client.get_request(address))
+    t2 = threading.Thread(target=client.post_request(address, 'b'))
+    t3 = threading.Thread(target=client.delete_request(address, 'b'))
+
     t1.start()
-    #t2.start()
-    #t3.start()
+    t2.start()
+    t3.start()
 
 
 if __name__ == "__main__":
     main()
+
+
